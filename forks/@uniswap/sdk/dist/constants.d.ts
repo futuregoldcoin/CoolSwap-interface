fda1cb01f2a7ddb2f1e3a473fa1d327ec0b8f47d
David Gomadza <welcome@gtps.finance>
2022-09-20 11:30:02 +0100

    G

--- forks/@uniswap/sdk/dist/constants.d.ts
+++ forks/@uniswap/sdk/dist/constants.d.ts
@@ -16,8 +16,8 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x6Bd5A1A63ffF10De3c6B7C667040E9AE1B47fDf2";
-export declare const INIT_CODE_HASH = "0xaf88dd15a55596feb9d67243c727bfd6144af12453963809bc91f0cfcf8241bc";
+export declare const FACTORY_ADDRESS = "0x6eeB4A7c1Cea697ee7BAF87de3D556bD200691eE";
+export declare const INIT_CODE_HASH = "0x0a6604cf955a46b83dab38f88aeabbfe06469a8eace1a55943e9de9ebce40657";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
